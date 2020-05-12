960b0d2fb8eee1a05c2abca367a4a2834effaf44
Roland Lauterbach <info@rolix.de>
2020-05-12 20:29:26 +0200

    fail2ban integration

--- init-poolsrv.sh
+++ init-poolsrv.sh
@@ -6,7 +6,7 @@
 # Define some variables
 
 ## directory of this file - absolute & normalized
-SCR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/resources/scripts"
+SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/prep_server"
 
 IS_EXTERNAL_HOST=''
 EXTERNAL_IP=''
@@ -31,26 +31,29 @@ function succ() {
 
 function check_programs() {
     [ -n "$(which curl)" ] || apt-get install -y curl &>/dev/null
-	[ $? -eq 0 ] || (warn 'Could not find or install curl'; abort 100;)
+	[ $? -eq 0 ] || { warn 'Could not find or install curl'; abort 100; }
 
     [ -n "$(which wget)" ] || apt-get install -y wget &>/dev/null
-	[ $? -eq 0 ] || (warn 'Could not find or install wget'; abort 100;)
+	[ $? -eq 0 ] || { warn 'Could not find or install wget'; abort 100; }
 
     [ -n "$(which dig)" ] || apt-get install -y dnsutils &>/dev/null
-	[ $? -eq 0 ] || (warn 'Could not find or install dig'; abort 100;)
+	[ $? -eq 0 ] || { warn 'Could not find or install dig'; abort 100; }
+
+    [ -n "$(which git)" ] || apt-get install -y git &>/dev/null
+	[ $? -eq 0 ] || { warn 'Could not find or install git'; abort 100; }
 }
 
 
 function configure_address() {
 
-    until [ "$IS_EXTERNAL_HOST" == "y" -o "$IS_EXTERNAL_HOST" == "n" ]
+    until [ "$IS_EXTERNAL_HOST" == "y" ] || [ "$IS_EXTERNAL_HOST" == "n" ]
     do
         read -p "Is this host directly connected to internet with a public ip (y/N) ? " -r IS_EXTERNAL_HOST
         if [ -z "$IS_EXTERNAL_HOST" ];then IS_EXTERNAL_HOST="n"; fi
     done
 
     if [ "$IS_EXTERNAL_HOST" == "y" ]; then
-        EX_IP=`curl -4 ifconfig.co`
+        EX_IP=$(curl -4 ifconfig.co)
         read -p "What is your external IP ($EX_IP)" -r EXTERNAL_IP
         if [ -z "$EXTERNAL_IP" ]; then EXTERNAL_IP=$EX_IP; fi
     else    
@@ -59,7 +62,7 @@ function configure_address() {
         done
     fi
 
-    EX_NAME=`dig -x $EXTERNAL_IP | grep -v ';' | grep 'PTR' | awk '{ print $5; }' | awk -F '.' '{ print $1"."$2"."$3; }'`
+    EX_NAME=$(dig -x "$EXTERNAL_IP" | grep -v ';' | grep 'PTR' | awk '{ print $5; }' | awk -F '.' '{ print $1"."$2"."$3; }')
     read -p "What is your external host name ($EX_NAME)" -r EXTERNAL_FQDN
     if [ -z "$EXTERNAL_FQDN" ]; then EXTERNAL_FQDN=$EX_NAME; fi
     
@@ -73,23 +76,61 @@ function configure_address() {
     [ "$IS_OK" != 'y' ] && (warn 'Faulty values'; abort 100;)
 }
 
+function install_fail2ban() {
+    apt-get update -y
+    apt-get install fail2ban -y
+    cat <<EOF >/etc/fail2ban/jail.d/jail-debian.local
+[sshd]
+port = 22
+maxretry = 3
+EOF
+    service fail2ban restart
+}
+
 function install_openvpn() {
-    [ -d $SRC/prep_server/scripts ] mkdir -p $SRC/prep_server/scripts
-    if [ ! -f $SRC/prep_server/scripts/openvpn-install.sh ]; then
-        wget -O $SRC/prep_server/scripts/openvpn-install.sh https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh &>/dev/null
+    [ -d "$SRC/scripts" ] || mkdir -p "$SRC/scripts"
+    [ -d "$SRC/openvpn" ] || mkdir -p "$SRC/openvpn"
+    if [ ! -f "$SRC/scripts/openvpn-install.sh" ]; then
+        wget -O "$SRC/scripts/openvpn-install.sh" https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh &>/dev/null
     fi
-    cp $SRC/prep_server/scripts/openvpn-install.sh /usr/local/sbin/
+    cp "$SRC/scripts/openvpn-install.sh" /usr/local/sbin/
     chmod +x /usr/local/sbin/openvpn-install.sh
+    chown www-data:www-data /usr/local/sbin/openvpn-install.sh
     export AUTO_INSTALL=y
-    if [ "$IS_EXTERNAL_HOST" == "N" ]; then export ENDPOINT=$EXTERNAL_IP; fi
-    /usr/local/sbin/openvpn-install.sh
+    if [ "$IS_EXTERNAL_HOST" == "n" ]; then export ENDPOINT=$EXTERNAL_IP; fi
+    inform "start installation openvpn server"
+    if /usr/local/sbin/openvpn-install.sh; then
+        /usr/local/sbin/openvpn-install.sh
+        touch "$SRC/openvpn/installed"
+        succ "openvpn server installed"
+    fi
+}
+
+function install_nginx() {
+    [ -d "$SRC/scripts" ] || mkdir -p "$SRC/scripts"
+    [ -d "$SRC/nginx" ] || mkdir -p "$SRC/nginx"
+    if [ ! -f "$SRC/scripts/nginx-install.sh" ]; then
+        wget -O "$SRC/scripts/nginx-install.sh" https://raw.githubusercontent.com/mietkamera/prep_servers/development/resources/nginx-install.sh &>/dev/null
+    fi
+    chmod +x "$SRC/scripts/nginx-install.sh"
+    # shellcheck source=resources/nginx-install.sh
+    "${SRC}"/scripts/nginx-install.sh
 }
 
 function main() {
-    echo -e "Welcome to \e[1mpool_srv_init\033[0m!\nThis will start the installation\nof pool server components on your system.\n"
+    # Make sure only root can run our script
+    if [[ $EUID -ne 0 ]]; then
+        error "This script must be run as root" 
+        exit 1
+    fi
+
+    echo -e "Welcome to \e[1minit-poolsrv\033[0m!\nThis will start the installation\nof pool server components on your system.\n"
 
     check_programs
     configure_address
+
+    install_fail2ban
+    install_openvpn
 
 }
 
---
+++ resources/nginx-install.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+#
+# Installation of nginx on debian 10
+# 
+
+function usage() {
+    cat 1>&2 <<EOF
+Install Script for nginx and Let's Encrypt SSL certificates
+version 0.1
+
+USAGE:
+    ./nginx-install [OPTIONS] FQDN
+
+OPTIONS:
+    -h, --help      Shows help dialog
+    -v, --version   Shows current version information
+    -f, --force     Overwrites old configuration
+
+FQDN:
+    fully qualified host name for example pool.basisit.com
+ 
+EOF
+}
+
+function version() {
+    cat 1>&2 <<EOF
+nginx-install       v0.1.1 unstable
+
+EOF
+}
+
+
+function install() {
+    if [ -n "$2" ] && [ "$2" == "f" ]; then FORCE=true; else FORCE=false; fi
+
+    apt-get -y update
+    for pak in nginx php php-fpm php-common php-mysql php-gmp php-curl php-mbstring php-intl php-xmlrpc php-gd php-imagick php-zip php-xml php-cli; do
+        apt-get install ${pak} -y
+    done
+    sed -i 's/memory_limit = 128M/memory_limit = 256M/g' /etc/php/7.3/fpm/php.ini
+    SRV=$1
+    TOOL=api
+    rm /var/www/html/* &>/dev/null
+    [ -L /etc/nginx/sites-enabled/default ] && unlink /etc/nginx/sites-enabled/default
+    [ -d /var/www/html/${TOOL} ] && ${FORCE} && rm -rf /var/www/html/${TOOL}
+    mkdir -p /var/www/html/${TOOL}
+    git clone https://github.com/mietkamera/pool_server_api /var/www/html/${TOOL}/
+    chown -R www-data:www-data /var/www/html
+    cat <<EOF > /etc/nginx/sites-available/${TOOL}
+server {
+    listen 80;
+    listen [::]:80;
+    server_name ${SRV};
+    include snippets/letsencrypt.conf;
+}
+EOF
+    apt-get install certbot -y
+    openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+
+    # now install Let's Encrypt SSL certificate by using webroot plugin
+    mkdir -p /var/lib/letsencrypt/.well-known
+    chgrp www-data /var/lib/letsencrypt
+    chmod g+s /var/lib/letsencrypt
+
+    # location for creating temporary file in webroot
+    cat <<EOF > /etc/nginx/snippets/letsencrypt.conf
+location ^~ /.well-known/acme-challenge/ {
+  allow all;
+  root /var/lib/letsencrypt/;
+  default_type "text/plain";
+  try_files \$uri =404;
+}
+EOF
+
+    # include recommended chiphers, enable OCSP stapling, HSTS and few security-focused HTTP headers
+    cat <<EOF > /etc/nginx/snippets/ssl.conf
+ssl_dhparam /etc/ssl/certs/dhparam.pem;
+
+ssl_session_timeout 1d;
+ssl_session_cache shared:SSL:10m;
+ssl_session_tickets off;
+
+ssl_protocols TLSv1.2 TLSv1.3;
+ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
+ssl_prefer_server_ciphers off;
+
+ssl_stapling on;
+ssl_stapling_verify on;
+resolver 8.8.8.8 8.8.4.4 valid=300s;
+resolver_timeout 30s;
+
+add_header Strict-Transport-Security "max-age=63072000" always;
+add_header X-Frame-Options SAMEORIGIN;
+add_header X-Content-Type-Options nosniff;
+EOF
+    ln -s /etc/nginx/sites-available/${TOOL} /etc/nginx/sites-enabled/${TOOL}
+    systemctl enable nginx
+    systemctl restart nginx
+    # now obtain SSL certificates
+    certbot certonly --agree-tos --email admin@example.com --webroot -w /var/lib/letsencrypt/ -d "${SRV}"
+
+    cat <<EOF > /etc/nginx/sites-available/${TOOL}
+server {
+    listen 80;
+    listen [::]:80;
+    server_name ${SRV};
+    access_log  off;
+    error_log   off;
+    return      301 https://\$server_name\$request_uri;
+}
+
+server {
+    listen 443 ssl http2;
+    listen [::]:443 ssl http2;
+    server_name ${SRV};
+    
+    ssl on;
+    ssl_certificate /etc/letsencrypt/live/${SRV}/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/${SRV}/privkey.pem;
+    ssl_trusted_certificate /etc/letsencrypt/live/${SRV}/chain.pem;
+    include snippets/ssl.conf;
+    include snippets/letsencrypt.conf;
+
+    access_log /var/log/nginx/${TOOL}.access.log;
+    error_log /var/log/nginx/${TOOL}.error.log error;
+
+    root /var/www/html/${TOOL};
+    index index.php;
+
+    location ~ \.php\$ {
+        fastcgi_index  index.php;
+        fastcgi_keep_conn on;
+        include        /etc/nginx/fastcgi_params;
+        fastcgi_pass   unix:/var/run/php/php7.3-fpm.sock;
+        fastcgi_param  SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+    }
+
+    location ~ / {
+        if (!-e \$request_filename) {
+          rewrite ^/(.*)\$ /index.php?url=\$1 last;
+        }
+    }
+}
+EOF
+    systemctl restart nginx
+}
+
+function main() {
+    case "$1" in
+        "--version" | "-v")
+            version
+            exit 1
+            ;;
+        "--help" | "-h")
+            help
+            exit 1
+            ;;
+        "--force" | "-f")
+            if [ -n "$2" ]; then
+                install "$2" "f"
+            else
+                echo "nginx-install.sh: you must provide a fqdn"
+                echo "See './nginx-install.sh -h'"
+                exit 1
+            fi
+            ;;
+        "")
+            echo "nginx-install.sh: you must provide a fqdn"
+            echo "See './nginx-install.sh -h'"
+            exit 1
+            ;;
+        *)
+            install "$1"
+            ;;
+    esac
+}
+
+main "$@" || exit 1
